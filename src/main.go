655848d1111e5d8c5a11a479374cde700a068661
昌维 <867597730@qq.com>
2020-06-13 02:14:43 +0800

    add main test file

--- src/main.go
+++ src/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"risc-v-compiler/compiler"
+	"risc-v-compiler/generator"
+	"risc-v-compiler/util"
+)
 
 func main() {
 	fmt.Println("RISC-V compiler Version 1.0.0")
 	fmt.Println("Author: cw1997 <chagnwei1006@gmail.com>")
 	fmt.Println("Repo: https://github.com/risc-v-cpu/risc-v-compiler")
 	fmt.Println()
+	run("./input.asm", "./output.hex")
+}
+
+func run(input string, output string) {
+	strArray := util.ReadFileToLinesWithoutLineBreak(input)
+	byteArray := compiler.CompileAll(strArray)
+	hexArray := generator.GenerateHex(byteArray)
+	util.WriteFilesByByteArray(output, hexArray)
 }
